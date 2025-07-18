890bd085028e9c872d27b2a099990ea81d3450d9
SeoMiYoung <112063987+SeoMiYoung@users.noreply.github.com>
2025-07-18 15:42:52 +0900

    Title: 시험 통과 여부 확인하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250718/시험 통과 여부 확인하기/verify-test-passed.java
+++ 250718/시험 통과 여부 확인하기/verify-test-passed.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.print("pass");
         }
         else {
-            System.out.printf("%d more score", 80-N+1);
+            System.out.printf("%d more score", 80-N);
         }
     }
 }
